73d09a94919bdfbead5d02d0412981434ca3f76d
Michael G Hein <michael.hein.dev@gmail.com>
2025-07-09 07:46:41 -0700

    Added more practice scripts.

--- rng/guessig_game.py
+++ rng/guessig_game.py
@@ -12,12 +12,13 @@ def main():
     #     Keep track of the number of guesses the player makes (starts at 0)
     #     Implement a main loop
     #         Have the player make a guess at what the number is
+    #         Add 1 to the guess counter
     #         If their guess is correct, end the game
-    #         Otherwise, add 1 to the guess counter and tell the player if their guess was too low or too high.
+    #         Otherwise, tell the player whether their guess was too low or too high.
     #     At the end, print out the player's score.
-    # Second, add the ability to replay the game without restarting the script, as well as a few other commands we will talk about in class.
+    # Second, add the ability to replay the game without restarting the script, as well as a few other commands we will talk about.
     #     This will involve nesting the main loop from the original version inside another loop.
-    #     We will talk about how to organize the code to make it easier.
+    #     We will discuss how to organize the code to make it easier.
     #     Additional commands include q to quit, r to restart, z to cheat and see the secret number, s to change the maximum secret to a different value than 100
     
     pass
