fc7f7550c8fb19bd352adceac7b032c8e498d648
Michael G Hein <mghdev@users.noreply.github.com>
2025-07-11 09:06:09 -0700

    New formatting.

--- misc_math/fibonacci.py
+++ misc_math/fibonacci.py
@@ -5,27 +5,40 @@
 #   fibonacci(1) is 1
 #   fibonacci(n) is fibonacci(n-1) + fibonacci(n-2)
 
-def fibonacci(n):
-    #=======================
-    # TODO
-    # Calculate the nth number in the Fibonacci sequence
-    # Return only the nth number in the sequence, not the entire sequence up to that point.
-    # Hints: 
-    # Use at least 3 variables to hold successive elements of the sequence
-    # Use a loop to advance through the sequence one number at a time
-    # Each time you want to advance the sequence, you can shuffle the values of your 3 variables around 
-    # to help you compute the next value without losing the old ones that you still need
-    
-    return 0
-    #=======================
 
+#=======================
+# TODO
+# Calculate the nth number in the Fibonacci sequence
+# Return only the nth number in the sequence, not the entire sequence up to that point.
+# Hints: 
+# Use at least 3 variables to hold successive elements of the sequence
+# Use a loop to advance through the sequence one number at a time
+# Each time you want to advance the sequence, you can shuffle the values of your 3 variables around 
+# to help you compute the next value without losing the old ones that you still need
+
+def fibonacci(n):
+    return 0
+
+#=======================
+
+
+
+#=======================
+# TODO
+# Call fibonacci() with some specific values to test it
+# For example:
+#   fibonacci(0) should return 1
+#   fibonacci(2) should return 2
+#   fibonacci(20) should return 10946
+#   fibonacci(40) should return 165580141
 
 def main():
-    assert fibonacci(0) == 1, "fibonacci failed test with n = 0"
-    assert fibonacci(2) == 2, "fibonacci failed test with n = 2"
-    assert fibonacci(20) == 10946, "fibonacci failed test with n = 20"
-    assert fibonacci(40) == 165580141, "fibonacci failed test with n = 40"
-    print("Passed all automated tests.")
+    print(fibonacci(0))
+
+#=======================
+
+
+
 
 if __name__ == "__main__":
     main()
